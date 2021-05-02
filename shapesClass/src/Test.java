c2588897dbf66f47bc59ed4f4c94648da304b5e6
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2021-05-02 22:59:10 +0530

    first

---
+++ shapesClass/src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void areaOfSquare(int one, int two) throws Exception {
+        System.out.println(one * two);
+    }
+}
---
+++ shapesClass/src/secondClass.java
@@ -0,0 +1,6 @@
+public class secondClass {
+    public static void main(String[] args) throws Exception {
+        Test Obj = new Test();
+        Obj.areaOfSquare(10, 20);
+    }
+}
