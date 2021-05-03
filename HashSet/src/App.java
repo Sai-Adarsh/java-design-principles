1c34d5af7def575d5ac183919343d3e18e59541b
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2021-05-03 17:58:31 +0530

    first

--- HashSet/src/App.java
+++ HashSet/src/App.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class App {
+    HashSet <Integer> visited = new HashSet <Integer>(5);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App Obj = new App();
+        for (int i = 0; i < 5; i++) {
+            Obj.visited.add(1);
+        }
+        System.out.println(Obj.visited.contains(1));
+        System.out.println(Obj.visited.contains(3));
+
+        Obj.visited.remove(1);
+        Obj.visited.clear();
     }
 }
