f66f09159cef215b38507d33ee95ccd9460be20f
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2021-06-08 03:05:32 +0530

    Encapsulation

---
+++ Design-and-OOPS/Encapsulation/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Bike Obj = new Bike();
+        Obj.setName("Yamaha MT15");
+        System.out.println(Obj.getName());
+    }
+}
---
+++ Design-and-OOPS/Encapsulation/src/Bike.java
@@ -0,0 +1,10 @@
+class Bike {
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String s) {
+        name = s;
+    }
+}
